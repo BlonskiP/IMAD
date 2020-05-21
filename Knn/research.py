e5d030a6441d1b4512faf05b9064609466365e6e
Piotr <Piotrblonski96@gmail.com>
2020-05-21 04:05:23 +0200

    results

--- Knn/research.py
+++ Knn/research.py
@@ -16,7 +16,7 @@ seeds = preprocess_dateset(seeds,['Type'],[])
 # print(s.predicts(iris,iris,3))
 #s.test(iris, 5)
 
-def reseach(vote,distance,folds,K,title):
+def reseach(vote,distance,folds,K,title,subtitle):
     results_col = {'Set':[],
                'precision':[],
                'recall':[],
@@ -24,16 +24,16 @@ def reseach(vote,distance,folds,K,title):
     results = pd.DataFrame(data=results_col)
     knn = Knn(distance,vote,K)
     print('---------IRIS ---------')
-    results= results.append(knn.test(iris,folds,'Iris'))
+    results= results.append(knn.test(iris,folds,'Iris'+subtitle))
     print('---------GLASS ---------')
     knn = Knn(distance, vote, K)
-    results = results.append(knn.test(glass,folds,'Glass'))
+    results = results.append(knn.test(glass,folds,'Glass'+subtitle))
     print('---------WINE ---------')
     knn = Knn(distance, vote, K)
-    results = results.append(knn.test(wine, folds, 'Wine'))
+    results = results.append(knn.test(wine, folds, 'Wine'+subtitle))
     print('---------Seeds ---------')
     knn = Knn(distance, vote, K)
-    results = results.append(knn.test(seeds, folds, 'seeds'))
+    results = results.append(knn.test(seeds, folds, 'seeds '+subtitle))
     filename = 'Knn-'+title+str(k)+'folds'+str(fold)+'.csv'
     results.to_csv(filename)
     print(results.to_latex())
@@ -42,19 +42,20 @@ K = [3, 5, 0]
 
 for fold in folds:
     for k in K:
+        subtitle = "K:"+str(k)+" Folds:"+str(fold)
         print('K:',k,' folds: ',fold)
         print('**Majority Vote')
         print('*******euclidean')
-        reseach(majority_vote,euclidean,fold,k,'majorityEuc')
+        reseach(majority_vote,euclidean,fold,k,'majorityEuc',subtitle+"majority "+"euclidean")
         print('*******manhattan')
-        reseach(majority_vote,manhattan,fold,k,'majority')
+        reseach(majority_vote,manhattan,fold,k,'majority',subtitle+"majority "+"manhattan")
         print('***distance_weighted_vote')
         print('*******euclidean')
-        reseach(distance_weighted_vote,euclidean,fold,k,'distanceEuc')
+        reseach(distance_weighted_vote,euclidean,fold,k,'distanceEuc',subtitle+"distance_weighted_vote "+"euclidean")
         print('*******manhattan')
-        reseach(distance_weighted_vote,manhattan,fold,k,'distanceMan')
+        reseach(distance_weighted_vote,manhattan,fold,k,'distanceMan',subtitle+"distance_weighted_vote "+"manhattan")
         print('***farest_distance_weighted_vote')
         print('*******euclidean')
-        reseach(farest_distance_weighted_vote,euclidean,fold,k,'FatestEuc')
+        reseach(farest_distance_weighted_vote,euclidean,fold,k,'FatestEuc',subtitle+"farest_distance_weighted_vote "+"euclidean")
         print('*******manhattan')
-        reseach(farest_distance_weighted_vote,manhattan,fold,k,'FatestMan')
+        reseach(farest_distance_weighted_vote,manhattan,fold,k,'FatestMan',subtitle+"farest_distance_weighted_vote "+"manhatan")
